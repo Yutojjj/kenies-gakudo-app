5b2bc61a658ba3f2a366a9e427fbfd40524a575c
Yutojjj <100813506+Yutojjj@users.noreply.github.com>
2026-08-21 15:31:50 +0900

    Update menu.tsx

--- app/menu.tsx
+++ app/menu.tsx
@@ -3401,7 +3401,7 @@ const styles = StyleSheet.create({
     width: '31.7%',
     minHeight: 108,
     borderRadius: 14,
-    backgroundColor: '#FFFDF9',
+    backgroundColor: '#EFF9FA',
     borderWidth: 1,
     borderColor: '#EAD9C7',
     alignItems: 'center',
